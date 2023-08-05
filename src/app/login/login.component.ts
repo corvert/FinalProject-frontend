a5a122d4db879f6f5bfdfc9b72f0deaacd7b3c9a
Orvet Priimägi <orvet.priimagi@gmail.com>
2023-08-05 22:32:51 +0300

    Security and Currency

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import{ AuthService } from '../service/AuthService'
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  username: string;
+  password: string;
+  errorMessage = 'Invalid Credentials';
+  successMessage: string;
+  invalidLogin = false;
+  loginSuccess = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+   
+  }
+
+  handleLogin() {
+    console.log(this.password, this.username);
+    this.authService.login(this.username, this.password).subscribe((result) => {
+      this.invalidLogin = false;
+      this.loginSuccess = true;
+      this.successMessage = 'Login Successful';
+      // redirect to main page
+    }, () => {
+      this.invalidLogin = true;
+      this.loginSuccess = false;
+    });
+  }
+
+}
